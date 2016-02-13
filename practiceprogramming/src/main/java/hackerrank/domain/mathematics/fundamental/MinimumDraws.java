23861dab70dc15961776e79033b8691515eca1d3
saranraj93 <saran_ssn@yahoo.com>
2016-02-13 13:23:28 +0530

    Push

---
+++ practiceprogramming/src/main/java/hackerrank/domain/mathematics/fundamental/MinimumDraws.java
@@ -0,0 +1,15 @@
+package hackerrank.domain.mathematics.fundamental;
+
+import java.util.Scanner;
+
+public class MinimumDraws {
+
+public static void main(String[] args){
+	Scanner sc=new Scanner(System.in);
+	int T=sc.nextInt();
+	for(int i=0;i<T;i++){
+		System.out.println(sc.nextInt()+1);
+	}
+}
+
+}
